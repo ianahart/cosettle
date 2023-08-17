78562382ed0d1fc7b786853cf13407eced263fa2
Ian Hart <ianhart7790@gmail.com>
2023-08-17 17:42:55 -0400

    feat: add global exceptions and their handlers

---
+++ src/main/java/com/hart/cosettle/advice/ForbiddenException.java
@@ -0,0 +1,10 @@
+package com.hart.cosettle.advice;
+
+
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
