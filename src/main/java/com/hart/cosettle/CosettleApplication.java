a19ae77ced705ba539718e17ba32dc5170c43f81
Ian Hart <ianhart7790@gmail.com>
2023-08-17 17:29:06 -0400

    feat: initial commit

---
+++ src/main/java/com/hart/cosettle/CosettleApplication.java
@@ -0,0 +1,13 @@
+package com.hart.cosettle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosettleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosettleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hart/cosettle/CosettleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hart.cosettle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosettleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
