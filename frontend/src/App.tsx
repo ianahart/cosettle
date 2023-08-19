df2616942673d2b377881f92f3dccbffd11cd53f
Ian Hart <ianhart7790@gmail.com>
2023-08-19 13:03:32 -0400

    feat: apply auth guards to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import HomeRoute from './routes/HomeRoute';
 import RegisterRoute from './routes/RegisterRoute';
 import LoginRoute from './routes/LoginRoute';
 import Footer from './components/Shared/Footer';
+import RequireGuest from './components/Guard/RequireGuest';
 
 function App() {
   return (
@@ -13,8 +14,22 @@ function App() {
         <Box minH="100vh">
           <Routes>
             <Route index element={<HomeRoute />} />
-            <Route path="/register" element={<RegisterRoute />} />
-            <Route path="/login" element={<LoginRoute />} />
+            <Route
+              path="/register"
+              element={
+                <RequireGuest>
+                  <RegisterRoute />
+                </RequireGuest>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RequireGuest>
+                  <LoginRoute />
+                </RequireGuest>
+              }
+            />
           </Routes>
         </Box>
       </Router>
