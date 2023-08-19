5f776435f10022f834829678ce3d035fb114702b
Ian Hart <ianhart7790@gmail.com>
2023-08-19 12:19:38 -0400

    feat: add endpoint for refreshing the user

---
+++ src/main/java/com/hart/cosettle/user/UserController.java
@@ -0,0 +1,37 @@
+package com.hart.cosettle.user;
+
+import com.hart.cosettle.user.dto.UserDto;
+import com.hart.cosettle.advice.NotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(path = "/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/sync")
+    public ResponseEntity<UserDto> syncUser(HttpServletRequest request) {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new NotFoundException("Invalid header token");
+        }
+
+        return ResponseEntity.status(200).body(this.userService.getUserByToken(authHeader.substring(7)));
+
+    }
+}
--- src/main/java/com/hart/cosettle/user/UserService.java
+++ src/main/java/com/hart/cosettle/user/UserService.java
@@ -1,18 +1,31 @@
 package com.hart.cosettle.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.hart.cosettle.advice.NotFoundException;
 import com.hart.cosettle.passwordreset.PasswordReset;
 import com.hart.cosettle.passwordreset.PasswordResetService;
 import com.hart.cosettle.passwordreset.request.PasswordResetRequest;
+import com.hart.cosettle.user.dto.UserDto;
+
+import java.security.Key;
+
 import com.hart.cosettle.advice.BadRequestException;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 @Service
 public class UserService {
+
+    @Value("${secretkey}")
+    private String secretKey;
 
     private final UserRepository userRepository;
     private final PasswordResetService passwordResetService;
@@ -51,5 +64,37 @@ public class UserService {
         user.setPassword(this.passwordEncoder.encode(request.getNewPassword()));
 
         this.userRepository.save(user);
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractUserIdFromToken(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    public UserDto getUserByToken(String token) {
+        Claims claims = extractUserIdFromToken(token);
+
+        User user = getUserByEmail(claims.getSubject());
+        UserDto userDto = new UserDto(
+                user.getId(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getRole(),
+                user.getAbbreviation(),
+                user.getLoggedIn(),
+                user.getProfile().getId());
+        return userDto;
+
     }
 }
