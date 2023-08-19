d0d2bd5fd3b08ad19901f2a523cb00048cbdc16a
Ian Hart <ianhart7790@gmail.com>
2023-08-19 13:01:34 -0400

    feat: add default state file

---
+++ frontend/src/state/initialState.ts
@@ -0,0 +1,15 @@
+export const tokenState = {
+  refreshToken: '',
+  token: '',
+};
+
+export const userState = {
+  abbreviation: '',
+  email: '',
+  id: 0,
+  lastName: '',
+  firstName: '',
+  loggedIn: false,
+  role: '',
+  profileId: 0,
+};
