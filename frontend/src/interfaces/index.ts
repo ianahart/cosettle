c359ccbbe199ca87418053518ef4ca0863dd9c70
Ian Hart <ianhart7790@gmail.com>
2023-08-22 14:25:27 -0400

    feat: add create space form state and interface

--- frontend/src/interfaces/index.ts
+++ frontend/src/interfaces/index.ts
@@ -39,3 +39,39 @@ export interface IUserContext {
     updateUser: (user: IUser) => void;
     logout: () => void;
 }
+
+
+export interface ICreateSpaceForm {
+    selectedIndex: number,
+    steps: {
+        description: {
+            size: {name: string, value: string, error: string, type: string},
+            capacity: {name: string, value: string, error: string, type: string},
+            location: {name: string, value: string, error: string, type: string},
+            description: {name: string, value: string, error: string, type: string},
+            flooring: {name: string, value: string, error: string, type: string},
+            wifi: {name: string, value: string, error: string, type: string}
+        },
+        availability: {
+            price: {name: string, value: string, error: string, type: string},
+            days: {name: string, value: string[], error: string, type: string}
+        },
+        amenities: {
+            bathrooms: {name: string, value: string, error: string, type: string},
+            utilities: {name: string, value: string, error: string, type: string},
+        },
+
+        contact: {
+            firstName: {name: string, value: string, error: string, type: string},
+            lastName: {name: string, value: string, error: string, type: string},
+            email: {name: string, value: string, error: string, type: string},
+            phoneNumber: {name: string, value: string, error: string, type: string},
+        }
+    }
+
+
+}
+
+
+
+
--- frontend/src/state/initialState.ts
+++ frontend/src/state/initialState.ts
@@ -29,3 +29,41 @@ export const userState = {
     theme: '',
     themeId: 0,
 };
+
+export const createSpaceState = {
+    selectedIndex: 0,
+    steps: {
+        description: {
+            size: {name: 'size', value: '', error: '', type: 'text'},
+            capacity: {name: 'capacity', value: '', error: '', type: 'text'},
+            location: {name: 'location', value: '', error: '', type: 'text'},
+            description: {name: 'description', value: '', error: '', type: 'text'},
+            flooring: {name: 'flooring', value: '', error: '', type: 'text'},
+            wifi: {name: 'wifi', value: '', error: '', type: 'text'}
+        },
+        availability: {
+            price: {name: 'price', value: '', error: '', type: 'text'},
+            days: {name: 'days', value: [], error: '', type: 'text'}
+        },
+        amenities: {
+            bathrooms: {name: 'bathroom', value: '', error: '', type: "checkbox"},
+            utilities: {name: 'utilities', value: '', error: '', type: "checkbox"},
+        },
+
+        contact: {
+            firstName: {name: 'firstName', value: '', error: '', type: 'text'},
+            lastName: {name: 'lastName', value: '', error: '', type: 'text'},
+            email: {name: 'email', value: '', error: '', type: 'email'},
+            phoneNumber: {name: 'phoneNumber', value: '', error: '', type: 'text'},
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
