ddf0781eb07ce25aa0cdc43e3fc49083584acb22
Ian Hart <ianhart7790@gmail.com>
2023-08-29 12:22:30 -0400

    chore: change heading text color

--- frontend/src/components/Shared/Logo.tsx
+++ frontend/src/components/Shared/Logo.tsx
@@ -15,7 +15,15 @@ const Logo = () => {
         src={user.theme === 'dark' || nonAuthTheme === 'dark' ? lightLogo : darkLogo}
         alt="logo of wifi bars"
       />
-            <Heading color={user.theme === 'dark' || nonAuthTheme === 'dark' ? 'text.primary': 'light.primary'} as="h1" fontSize="1.1rem">
+      <Heading
+        color={
+          user.theme === 'dark' || nonAuthTheme === 'dark'
+            ? 'text.primary'
+            : 'light.primary'
+        }
+        as="h1"
+        fontSize="1.1rem"
+      >
         CoSettle
       </Heading>
     </Flex>
