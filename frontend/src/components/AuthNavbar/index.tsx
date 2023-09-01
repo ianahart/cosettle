af2495b7dc832568c4784f0e3df2a51697c32805
Ian Hart <ianhart7790@gmail.com>
2023-09-01 11:00:51 -0400

    feat: wrap logo in an anchor tag pointing to home

--- frontend/src/components/AuthNavbar/index.tsx
+++ frontend/src/components/AuthNavbar/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex } from '@chakra-ui/react';
 import Logo from '../Shared/Logo';
 import Account from '../Account';
+import { Link as RouterLink } from 'react-router-dom';
 import ThemeToggler from '../Shared/ThemeToggler';
 import { useContext } from 'react';
 import { UserContext } from '../../context/user';
@@ -12,7 +13,9 @@ const AuthNavbar = () => {
     <Box bg={user.theme === 'dark' ? '#161515' : 'primary.dark'} minH="40px" p="0.25rem">
       <Flex justifyContent="space-between" alignItems="center">
         <Box>
-          <Logo />
+          <RouterLink to="/">
+            <Logo />
+          </RouterLink>
         </Box>
         <Flex justifyContent="space-between" alignItems="center">
           <Box mr="2rem">
